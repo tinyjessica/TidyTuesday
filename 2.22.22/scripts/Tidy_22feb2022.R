6d1e7d2adaae73a357da1e05d9b72869b99cc4e7
tinyjessica <tinkerbell404@gmail.com>
2022-05-06 23:45:39 -0700

    not finished but sending update

--- 2.22.22/scripts/Tidy_22feb2022.R
+++ 2.22.22/scripts/Tidy_22feb2022.R
@@ -1,16 +1,17 @@
-#################################
+#############################################################################
 # Created 2-28-2022
 # Author Jessica Hunter
 # This Tidy Tuesday is about Freedom
 # this data is from 195 countries and their civil rights and liberties
-###########################################
+#############################################################################
 
 library(tidyverse)
 library(here)
 library(ggplot2)
 library(gganimate)
 
-# insta
+#############################################################################
+
 # install.packages("gganimate")
 # install.packages("tidytuesdayR")
 # tuesdata <- tidytuesdayR::tt_load('2022-02-22')
@@ -36,12 +37,12 @@ anim = staticplot +
   transition_states(year,
                     transition_length = 4,
                     state_length = 1)+
-  labs(title = "Number of not-free or partially-free Countries by Region {closest_state}")
+  labs(title = "Number of Countries not fully free by Region {closest_state}")
 
 animate(anim, 20, fps = 2,
-        width = 1200,
-        height = 1000,
-        renderer = gifski_renderer("gganim.gif")) 
+        width = 1000, # in pixels
+        height = 800,
+        renderer = gifski_renderer("gganim.gif"))
 
 
 
